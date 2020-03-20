50cdbe55b285d12a52bec9e4d187e39324190589
xuel12 <xue_xl@hotmail.com>
2020-03-20 15:44:14 -0400

    add model construct

--- local_training/training_gcp_cpu.py
+++ local_training/training_gcp_cpu.py
@@ -2,7 +2,7 @@ import io_local
 import losses
 import model as model_lib
 import constants
-import constants_gcp
+import constants_gcp_cpu
 
 import os
 from keras.utils import plot_model
@@ -104,9 +104,9 @@ def train(tensor, model, model_config, callbacks):
         
 if __name__ == "__main__":
     os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # turn off tf logging
-    os.chdir(constants_gcp.BASE_PATH + 'project/prosit/local_training')
-    data_path = constants_gcp.DATA_PATH
-    model_dir = constants_gcp.MODEL_DIR
+    os.chdir(constants_gcp_cpu.BASE_PATH + 'project/prosit/local_training')
+    data_path = constants_gcp_cpu.DATA_PATH
+    model_dir = constants_gcp_cpu.MODEL_DIR
     model, model_config = model_lib.load(model_dir, trained=False)
    
     # create log folder
